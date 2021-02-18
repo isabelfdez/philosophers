eda3d6d036c825792ddfae2e66d1e957129824b3
isfernan <isabel.fernandez.abad@gmail.com>
2021-02-18 19:00:59 +0100

    check arguments

--- philo_one/main.c
+++ philo_one/main.c
@@ -1,19 +1,97 @@
 
+#include "philo_one.h"
+
+int n = 0;
+
 int main(int argc, char **argv)
 {
-    int     num_phil;
-    int     t_eat,
+	t_philo *p;
 
-    if (argc < 5)
-    {
-        write(1, "too few arguments\n", strlen("too few arguments\n"));
-        return (-1);
+	if (!check_arguments(argc, argv))
+		return (-1);
+	p = malloc(sizeof(t_philo));
+	p->num = atoi(argv[1]);
+	n = p->num;
+	printf("el n vale ahora %i\n", n);
+	p->tdie = atoi(argv[2]);
+	p->teat = atoi(argv[3]);
+	p->tsleep = atoi(argv[4]);
+	if (argc == 6)
+		p->meals = atoi(argv[5]);
+	else
+		p->meals = -1;
+	create_threads(p);
+}
+
+int		check_arguments(int argc, char **argv)
+{
+	if (argc < 5)
+	{
+		write(1, "too few arguments\n", strlen("too few arguments\n"));
+		return (0);
+	}
+	if (argc > 6)
+	{
+		write(1, "too many arguments\n", strlen("too many arguments\n"));
+		return (0);
+	}
+	if (atoi(argv[1]) <= 0 || atoi(argv[2]) <= 0 || atoi(argv[3]) <= 0 ||
+		atoi(argv[4]) <= 0)
+	{
+		write(1, "invalid arguments\n", strlen("invalid arguments\n"));
+		return (0);
+	}
+	if (argc == 6 && atoi(argv[5]) <= 0)
+	{
+		write(1, "invalid arguments\n", strlen("invalid arguments\n"));
+		return (0);
+	}
+	return (1);
+}
+
+void	create_threads(t_philo *p)
+{
+	pthread_t	*threads;
+	long		i;
+	int			j;
+
+	i = 0;
+	j = 0;
+	threads = malloc(sizeof(pthread_t) * p->num);
+	while (i < p->num)
+	{
+		 pthread_create(&threads[i], NULL, start_tasks, (void *)i);
+		 i++;
+	}
+	while (j < p->num)
+	{
+        pthread_join(threads[j], NULL);
+		j++;
+	}
+}
+
+void	*start_tasks(void *i)
+{
+	pthread_mutex_t mutex;
+    //mutex = malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(&mutex, NULL);
+    while (TRUE) 
+	{
+        pthread_mutex_lock(&mutex);
+
+        // check if all COUNT_TO has been arrived at
+        if (i >= COUNT_TO) {
+            pthread_mutex_unlock(&mutex);
+            return NULL;
+        }
+
+        ++i;
+
+        // release lock
+        pthread_mutex_unlock(&mutex);
+
+        printf("i = %lld\n", i);
     }
-    if (argc > 6)
-    {
-        write(1, "too many arguments\n", strlen("too many arguments\n"));
-        return (-1);
-    }
-    num_phil = atoi(argv[1]);
-    printf("philos: %i\n", num_phil);
+    pthread_mutex_destroy(&mutex);
+    //free(mutex);
 }
--- philo_one/philo_one.h
+++ philo_one/philo_one.h
@@ -1,10 +1,26 @@
-#ifndef
+#ifndef PHILO_ONE_F
+# define PHILO_ONE_F
 
-#include <stdio.h>
-#include <sys/time.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <string.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <string.h>
 
-struct  philo
+typedef struct  s_philo
+{
+	int		num;
+	int		tdie;
+	int		teat;
+	int		tsleep;
+	int		meals;
+}				t_philo;
+
+int		check_arguments(int argc, char **argv);
+void	create_threads(t_philo *p);
+void	*start_tasks(void *i);
+
+
+
+#endif
